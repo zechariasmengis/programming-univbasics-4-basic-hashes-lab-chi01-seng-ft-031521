18486d4def43bbf7e972a3f011968b74ef3ae36e
Zecharias Mengis <zmengis@gmail.com>
2021-02-11 18:22:12 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,23 @@
 def new_hash
-  # return an empty hash
+  return {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  monkas = {
+    pog: "pogerino"
+  }
+  return monkas
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  return {
+    name: "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  return {
+    id: number
+  }
 end
